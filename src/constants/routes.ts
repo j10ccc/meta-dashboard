016a1f839145da2a342350cccb317d5cb62d727a
j10c <blyb1739@gmail.com>
2024-01-19 15:08:32 +0800

    refactor(import): add baseUrl

---
+++ src/constants/routes.ts
@@ -0,0 +1,27 @@
+export default [
+  {
+    title: "Overview",
+    path: `${import.meta.env.BASE_URL}/overview`,
+    icon: "i-fluent-mdl2-view-dashboard"
+  },
+  {
+    title: "Proxies",
+    path: `${import.meta.env.BASE_URL}/proxies`,
+    icon: "i-fluent-mdl2-globe"
+  },
+  {
+    title: "Connections",
+    path: `${import.meta.env.BASE_URL}/connections`,
+    icon: "i-fluent-mdl2-plug-connected"
+  },
+  {
+    title: "Logs",
+    path: `${import.meta.env.BASE_URL}/logs`,
+    icon: "i-fluent-mdl2-document-set"
+  },
+  {
+    title: "Settings",
+    path: `${import.meta.env.BASE_URL}/settings`,
+    icon: "i-fluent-mdl2-settings"
+  }
+];
