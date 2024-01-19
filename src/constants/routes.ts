01e6248ab63a2554c96bc294bc417bd7e9c7ed3c
j10c <blyb1739@gmail.com>
2024-01-19 23:51:18 +0800

    fix(navigator): use static render for icon class

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,27 +1,31 @@
-export default [
+export type RouteTitle = "Overview" | "Proxies" | "Connections" | "Logs" | "Settings";
+
+export type Route = {
+  title: RouteTitle;
+  path: string;
+}
+
+const routes: Route[] = [
   {
     title: "Overview",
     path: `${import.meta.env.BASE_URL}/overview`,
-    icon: "i-fluent-mdl2-view-dashboard"
   },
   {
     title: "Proxies",
     path: `${import.meta.env.BASE_URL}/proxies`,
-    icon: "i-fluent-mdl2-globe"
   },
   {
     title: "Connections",
     path: `${import.meta.env.BASE_URL}/connections`,
-    icon: "i-fluent-mdl2-plug-connected"
   },
   {
     title: "Logs",
     path: `${import.meta.env.BASE_URL}/logs`,
-    icon: "i-fluent-mdl2-document-set"
   },
   {
     title: "Settings",
     path: `${import.meta.env.BASE_URL}/settings`,
-    icon: "i-fluent-mdl2-settings"
   }
 ];
+
+export default routes;
