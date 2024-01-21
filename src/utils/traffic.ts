a3ca8c1bcddd906b0f942fe13cdafd8c89fe5018
j10c <blyb1739@gmail.com>
2024-01-21 17:56:00 +0800

    perf(traffic): update unit display strategy

--- src/utils/traffic.ts
+++ src/utils/traffic.ts
@@ -1,13 +1,13 @@
 import { RateUnitEnum } from "@/interfaces/Traffic";
 
 export function convertRateUnit(value: number): { value: string, unit: RateUnitEnum } {
-  if (value > 1024 * 1024 * 1024) {
-    return { value: (value / 1024 / 1024 / 1024).toFixed(1), unit: RateUnitEnum.GB };
-  } else if (value > 1024 * 1024) {
-    return { value: (value / 1024 / 1024).toFixed(1), unit: RateUnitEnum.MB };
-  } else if (value > 1024) {
-    return { value: (value / 1024).toFixed(1), unit: RateUnitEnum.KB };
-  } else {
+  if (value < 1000) {
     return { value: value.toString(), unit: RateUnitEnum.B };
+  } else if (value < 1000 * 1000) {
+    return { value: (value / 1024).toFixed(1), unit: RateUnitEnum.KB };
+  } else if (value < 1000 * 1000 * 1000) {
+    return { value: (value / 1024 / 1024).toFixed(1), unit: RateUnitEnum.MB };
+  } else {
+    return { value: (value / 1024 / 1024 / 1024).toFixed(1), unit: RateUnitEnum.GB };
   }
 }
